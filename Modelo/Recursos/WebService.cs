ce21141d52b0e025e4b27d7636ac266d0b4421a0
Dennys Chillca <dennyx.cc@gmail.com>
2018-04-21 09:57:02 -0500

    comentario forzado

--- Modelo/Recursos/WebService.cs
+++ Modelo/Recursos/WebService.cs
@@ -24,7 +24,7 @@ namespace Modelo.Recursos
         {
             //this.domainName = "http://www.lineatienda.com";
 
-            this.domainName = "http://192.168.1.17:8080";
+            this.domainName = "http://192.168.1.17:8085";
             //this.domainName = "http://localhost:8080";
             //a
 
